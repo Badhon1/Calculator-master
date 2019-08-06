4955dd17d8ccb0ec786b2dd952dbfba669f35f8c
[Tanvir Hossain] <[iamtanvir.cse@gmail.com]>
2019-08-06 22:23:07 +0600

    here the new option is add named divider for calculation

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,7 @@ public class Calculator{
 		System.out.println(addition(3,4));
 		System.out.println(subtraction(13,4));
 		System.out.println(multiplication(4,5));
+		System.out.println(divider(8,2));
 	}
 
 	double addition(double x, int double){
@@ -15,5 +16,8 @@ public class Calculator{
 	}
 	double multiplication(double x, double y){
 		return x * y;
+	}
+	double divider(double x, double y){
+		return x / y;
 	}
 }
